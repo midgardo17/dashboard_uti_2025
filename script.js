45607be75a830f7b95f33a11369c5deaa5b2c446
midgardo17 <thiagobcmoreira@gmail.com>
2025-03-05 22:08:57 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const data = {
+    "Mortalidade ajustada": {
+        "Jan": "-1%",
+        "Fev": "0.01%",
+        "Mar": "NA",
+        "Abr": "NA",
+        "Mai": "NA",
+        "Jun": "NA",
+        "Jul": "NA",
+        "Ago": "NA",
+        "Set": "NA",
+        "Out": "NA",
+        "Nov": "NA",
+        "Dez": "NA",
+        "Meta Alcançada": "0%",
+        "Meta Estipulada": "0%"
+    },
+    "Necessidade de cuidados de enfermagem (NAS)": {
+        "Jan": "70%",
+        "Fev": "75%",
+        "Mar": "NA",
+        "Abr": "NA",
+        "Mai": "NA",
+        "Jun": "NA",
+        "Jul": "NA",
+        "Ago": "NA",
+        "Set": "NA",
+        "Out": "NA",
+        "Nov": "NA",
+        "Dez": "NA",
+        "Meta Alcançada": "80%",
+        "Meta Estipulada": "80%"
+    }
+    // Adicione os outros indicadores aqui
+};
